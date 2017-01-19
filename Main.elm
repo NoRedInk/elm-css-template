fe592925386e71fa8540d56bf11869c7cef07406
Aaron VonderHaar <av@noredink.com>
2017-01-18 16:51:05 -0800

    Basic project template for playing with elm-css

---
+++ Main.elm
@@ -0,0 +1,89 @@
+module Main exposing (main)
+
+import Css exposing (..)
+import Css.Namespace exposing (namespace)
+import Css.Elements exposing (body, li)
+import Html exposing (Html)
+import Html.CssHelpers
+
+
+{-| In this template, define your desired HTML here in `page`.  There is a `main` defined
+at the bottom of this file that uses `page` and attaches the compiled Elm CSS.
+-}
+page : Html msg
+page =
+    Html.div [ id Page ]
+        [ Html.div [ class [ NavBar ] ] [ Html.text <| toString <| Css.compile [ css ] ]
+        , Html.p [] [ Html.text "TODO" ]
+        ]
+
+
+{-| Define the Elm CSS classes you want to use
+-}
+type CssClasses
+    = NavBar
+
+
+{-| Defined any HTML IDs you want to use.  (You should probably use classes above instead.)
+-}
+type CssIds
+    = Page
+
+
+css : Stylesheet
+css =
+    (stylesheet << namespace "Main-")
+        [ body
+            [ overflowX auto
+            , minWidth (px 1280)
+            ]
+        , (#) Page
+            [ backgroundColor (rgb 200 128 64)
+            , color (hex "CCFFFF")
+            , width (pct 100)
+            , height (pct 100)
+            , boxSizing borderBox
+            , padding (px 8)
+            , margin zero
+            ]
+        , (.) NavBar
+            [ margin zero
+            , padding zero
+            , children
+                [ li
+                    [ (display inlineBlock) |> important
+                    , color primaryAccentColor
+                    ]
+                ]
+            ]
+        ]
+
+
+primaryAccentColor : Color
+primaryAccentColor =
+    hex "ccffaa"
+
+
+
+-- -- --
+-- -- --
+-- -- --  Everything below here is boilerplate code to set up Elm CSS
+-- -- --
+-- -- --
+
+
+{ id, class, classList } =
+    Html.CssHelpers.withNamespace "Main-"
+
+
+attachElmCssStyles : Html msg
+attachElmCssStyles =
+    Html.CssHelpers.style <| .css <| Css.compile [ css ]
+
+
+main : Html msg
+main =
+    Html.div []
+        [ attachElmCssStyles
+        , page
+        ]
---
+++ README.md
@@ -0,0 +1,19 @@
+
+## How to use
+
+In your Terminal app, go to the folder where this file is.
+You can easily do that by typing `cd ` (with a trailing space), and then dragging the folder into the terminal.
+
+Then start elm reactor:
+
+```sh
+elm-reactor
+```
+
+Then open your web browser to [http://localhost:8000/Main.elm](http://localhost:8000/Main.elm)
+Whenever you reload the page, the Elm files will be recompiled (and will show you if there are any errors).
+
+## More info
+
+ - Elm CSS: http://package.elm-lang.org/packages/rtfeldman/elm-css/latest/
+ - The HTML-to-Elm tool: https://mbylstra.github.io/html-to-elm/
